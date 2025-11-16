bb2f0adb82b022d309361d6e94ab4301838688d8
Emnet Befkadu <emnbef456@gmail.com>
2025-11-16 23:22:59 +0300

    sa

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -24,8 +24,22 @@
 // }
 
 
+// import { Stack } from "expo-router";
+// import { View } from 'react-native'
+
+// export default function RootLayout() {
+//   return (
+//     <Stack screenOptions={{
+//       headerShown: false,
+//     }}>
+//       <Stack.Screen name="/" />
+//       <Stack.Screen name="join" />
+//       <Stack.Screen name="signin" />
+//     </Stack>
+//   );
+// }
+
 import { Stack } from "expo-router";
-import { View } from 'react-native'
 
 export default function RootLayout() {
   return (
@@ -37,4 +51,4 @@ export default function RootLayout() {
       <Stack.Screen name="signin" />
     </Stack>
   );
-}
+} 
